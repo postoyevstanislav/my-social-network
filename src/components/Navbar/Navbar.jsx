3d5da7ce72aec031fb4b9b63e8fe1fd9c564b522
staspostoyev <mr.postoyev@gmail.com>
2020-04-18 16:25:07 +0200

    add react-router-dom and pages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import classes from './Navbar.module.css'
- console.log(classes)
+import {NavLink} from "react-router-dom";
+import style from './Navbar.module.css'
+
+console.log(style)
 const Navbar = () => {
-    return(
-        <nav className={classes.nav}>
-            <div className={classes.item}><a>Profile</a></div>
-            <div className={`${classes.item} ${classes.active}`}><a>Messages</a></div>
-            <div className={classes.item}><a>News</a></div>
-            <div className={classes.item}><a>Music</a></div>
-            <div className={classes.item}><a>Settings</a></div>
+    return (
+        <nav className={style.nav}>
+            <div className={style.item}>
+                <NavLink to='/profile' activeClassName={style.activeLink}>Profile</NavLink>
+            </div>
+            <div className={`${style.item} `}>
+                <NavLink to='/dialogs' activeClassName={style.activeLink}>Messages</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to='/news' activeClassName={style.activeLink}>News</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to='/music' activeClassName={style.activeLink}>Music</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to='/settings' activeClassName={style.activeLink}>Settings</NavLink>
+            </div>
         </nav>
     )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import classes from './Profile.module.css'
+import style from './Profile.module.css'
 import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
-    return(
-        <div className={classes.content}>
+    return (
+        <div>
             <div>
                 <img src="https://i.pinimg.com/originals/e7/e9/1f/e7e91f5fef6726ff99a620202694340e.jpg" alt=""/>
             </div>
             <div>
                 ava + description
             </div>
-            <MyPosts />
+            <MyPosts/>
         </div>
     )
 }
